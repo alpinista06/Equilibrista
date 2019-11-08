0aa729b48e948e48f503c9e1128d9c4e9aa81ee2
alpinista06 <mariod.b.filho@gmail.com>
2019-11-08 11:12:29 -0300

    Retirando a ação derivativa do controlador por conta de o sinal ser muito ruidoso.

--- compensador.ino
+++ compensador.ino
@@ -1,11 +1,12 @@
 double Kp = 30;
-double Kd = 5;
+//double Kd = 5;
 double Ki = 5;
 
 double resposta_p = 0;
-double resposta_d = 0;
+//double resposta_d = 0;
 double resposta_i = 0;
 double Resposta_PID = 0;
+double Resposta_PI = 0;
 double erro = 0;
 double amostra_atual = 0;
 double amostra_anterior = 0;
@@ -16,15 +17,17 @@ float set_point = -3;
 double compensador(double controle)
 {
 Resposta_PID = 0;
-  
+Resposta_PI = 0;
+
 erro = set_point - controle;
 resposta_p = erro * Kp;
 resposta_i = resposta_i + erro * Ki;
 resposta_i = resposta_i < 0 ? 0 : resposta_i;
 resposta_i = resposta_i > 255 ? 255 : resposta_i;
 
-resposta_d = Kd * (erro - erro_anterior);
-Resposta_PID = resposta_p + resposta_i + resposta_d;
+//resposta_d = Kd * (erro - erro_anterior);
+//Resposta_PID = resposta_p + resposta_i + resposta_d;
+Resposta_PI = resposta_p + resposta_i
 erro_anterior = erro;
 controle = abs(Resposta_PID);
 
@@ -34,9 +37,9 @@ Serial.print(resposta_p);
 Serial.print("\t");
 Serial.print(resposta_i);
 Serial.print("\t");
-Serial.print(resposta_d);
-Serial.print("\t");
-Serial.println(Resposta_PID);
+//Serial.print(resposta_d);
+//Serial.print("\t");
+Serial.println(Resposta_PI);
 #endif
 
 return controle;
